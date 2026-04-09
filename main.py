4b3a455610563f49bf85b44241bb5552e756dfeb
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-09 11:02:52 -0300

    main

---
+++ main.py
@@ -0,0 +1,38 @@
+# Preparando o ambiente:
+import sympy
+
+from sympy import symbols, simplify
+
+#Deinir as variáveis:
+x, y, z, a, b, c = symbols('x y z a b c')
+
+#a)
+#expr_a = (3*x + 2*y - 5) + (4*x - y + 8)
+#print(simplify(expr_a))
+
+#expr_b = (5*x**2 - x + 1) - (2*x**2 + 4*x - 3)
+#print(simplify(expr_b))
+
+#expr_c = 7*z**2 - (3*z**2 - 5*z + 1)
+#print(simplify(expr_c))
+
+#expr_d = (6*a**2 - 3*b) - (2*a**2 + 5*b - 7)
+#print(simplify(expr_d))
+
+#expr_e = (-2*a + 5*b - 3*c) + (7*a - b + 2*b)
+#print(simplify(expr_e))
+
+expr_f = (-8*x**3) * (4*x)
+print(simplify(expr_f))
+
+expr_g = 5*a**4*x**3 * 2*a*x**4
+print(simplify(expr_g))
+
+expr_h = 12*y**5 / 4*y**3
+print(simplify(expr_h))
+
+expr_i = 20*a**4*b**2 / (-5*a*b)
+print(simplify(expr_i))
+
+expr_j = (3*x**2*y) * (2*x**3)
+print(simplify(expr_j))
